8e8fc1f9c961d0fc316fb689ead75a11f493ebda
Joe0fficial <139757196+Joe0fficial@users.noreply.github.com>
2023-08-13 06:15:48 -0700

    Create Materials.lua

---
+++ Materials.lua
@@ -0,0 +1,13 @@
+return {
+    ["ForceField"] = Enum.Material.ForceField,
+    ["Asphalt"] = Enum.Material.Asphalt,
+    ["Brick"] = Enum.Material.Brick,
+    ["Cobblestone"] = Enum.Material.Cobblestone,
+    ["Glass"] = Enum.Material.Cobblestone,
+    ["Ice"] = Enum.Material.Ice,
+    ["Metal"] = Enum.Material.Metal,
+    ["Plastic"] = Enum.Material.Plastic,
+    ["Wood"] = Enum.Material.Wood,
+    ["Wood Planks"] = Enum.Material.WoodPlanks,
+    ["CrackedLava"] = Enum.Material.CrackedLava,
+}
